7dd98ea113413a0e18c9658ea39902cf3c7d53cb
Shreyas <93078655+sh3y@users.noreply.github.com>
2022-01-25 04:02:39 -0600

    smh

--- middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = function(req, res, next) {
-   console.log(chalk.green('[LOG]'), chalk.blue(req.method), `Request From IP ${chalk.yellow(req.ip)}`, chalk.blue(req.originalUrl));
-
-    next();
-}
